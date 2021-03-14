b581c49739ee84805681cfeda7f1a59319510dc0
Alex Francys <francys_alex2007@hotmail.com>
2021-03-14 11:51:06 -0300

    Programa Finalizado

---
+++ ContaBancaria/Classes/Conta.cs
@@ -0,0 +1,61 @@
+﻿using ContaBancaria.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria.Classes
+{
+    class Conta
+    {
+        private string Nome { get; set; }
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+
+        private double ChequeEspecial { get; set; }
+
+        public Conta(string nome, TipoConta tipoConta, double saldo, double chequeEspecial)
+        {
+            Nome = nome;
+            TipoConta = tipoConta;
+            Saldo = saldo;
+            ChequeEspecial = chequeEspecial;
+        }
+
+        public void Saque(double valorSaque)
+        {
+            if (this.Saldo < valorSaque)
+            {
+                Console.Write("Saldo insuficiente, deseja retirar do Credito Especial? S ou N? ");
+                char opcao = char.Parse(Console.ReadLine().ToUpper());
+                if (opcao == 'S')
+                {
+                    this.Saldo = (this.Saldo + this.ChequeEspecial) - valorSaque;
+                }
+            }
+            else
+            {
+                this.Saldo -= valorSaque;
+            }
+            Console.WriteLine("O Saldo da conta de {0} é de {1}", this.Nome, this.Saldo);
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+            Console.WriteLine("O Saldo da conta de {0} é de {1}", this.Nome, this.Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            this.Saque(valorTransferencia);
+            contaDestino.Depositar(valorTransferencia);
+
+        }
+
+        public override string ToString()
+        {
+            string retorno = "A conta de " + this.Nome + " do tipo " + this.TipoConta + " está atualmente com o saldo de " + this.Saldo + ".";
+            return retorno;
+        }
+    }
+}
--- ContaBancaria/Program.cs
+++ ContaBancaria/Program.cs
@@ -1,12 +1,106 @@
-﻿using System;
+﻿using ContaBancaria.Classes;
+using ContaBancaria.Enums;
+using System;
+using System.Collections.Generic;
 
 namespace ContaBancaria
 {
     class Program
     {
+        static List<Conta> listaContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao = 0;
+            int numeroConta = 0;
+
+            while (opcao != 6)
+            {
+                Console.WriteLine("Conta Bancaria");
+                Console.WriteLine("O que deseja fazer:");
+                Console.WriteLine("1 - Adicionar Conta");
+                Console.WriteLine("2 - Depositar");
+                Console.WriteLine("3 - Sacar");
+                Console.WriteLine("4 - Transferir");
+                Console.WriteLine("5 - Listar Contas");
+                Console.WriteLine("6 - Sair");
+                Console.Write("\nSua opção: ");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        AdicionarConta();
+                        break;
+
+                    case 2:
+                        Console.Write("Qual o Numero da Conta: ");
+                        numeroConta = int.Parse(Console.ReadLine());
+                        
+                        Console.Write("Quanto deseja depositar: ");
+                        listaContas[numeroConta].Depositar(double.Parse(Console.ReadLine()));
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        Console.Write("Qual o Numero da Conta: ");
+                        numeroConta = int.Parse(Console.ReadLine());
+
+                        Console.Write("Quanto deseja Sacar: ");
+                        listaContas[numeroConta].Saque(double.Parse(Console.ReadLine()));
+                        Console.WriteLine();
+                        break;
+
+                    case 4:
+                        Console.Write("Qual o Numero da Conta: ");
+                        numeroConta = int.Parse(Console.ReadLine());
+
+                        Console.Write("Qual o Numero da Conta de Destino: ");
+                        int numeroContaDestino = int.Parse(Console.ReadLine());
+
+
+                        Console.Write("Quanto deseja transferir: ");
+                        listaContas[numeroConta].Transferir(double.Parse(Console.ReadLine()), listaContas[numeroContaDestino]);
+
+                        break;
+
+                    case 5:
+                        Console.WriteLine();
+                        int i = 0;
+                        foreach (Conta x in listaContas) { 
+                        Console.WriteLine("Conta Nº: " + i + " " + x.ToString());
+                        i++; 
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        private static void AdicionarConta()
+        {
+
+            Console.WriteLine("\nAdicionar nova conta");
+
+            Console.Write("Digite 1 para Conta Fisica ou 2 para Juridica: ");
+            int tipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Nome do Cliente: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o saldo inicial: ");
+            double saldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do Cheque Especial: ");
+            double chequeEspecial = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(nome, (TipoConta)tipoConta, saldo, chequeEspecial);
+
+            listaContas.Add(novaConta);
+
         }
     }
 }
